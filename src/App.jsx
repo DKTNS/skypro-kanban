5fba8e61c39dabbe80dd28d58fcacbb5fa49a00e
DKTNS <zigindmitry@gmail.com>
2024-02-18 00:54:06 +0300

    Перенесены:разметка,стили,шрифты

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,12 @@ function App() {
               </div>
               <form class="pop-exit__form" id="formExit" action="#">
                 <div class="pop-exit__form-group">
-                  <button class="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
-                  <button class="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
+                  <button className="pop-exit__exit-yes _hover01" id="exitYes">
+                    <a href="modal/signin.html">Да, выйти</a>{" "}
+                  </button>
+                  <button className="pop-exit__exit-no _hover03" id="exitNo">
+                    <a href="main.html">Нет, остаться</a>{" "}
+                  </button>
                 </div>
               </form>
             </div>
@@ -36,12 +40,25 @@ function App() {
                 <div class="pop-new-card__wrap">
                   <form class="pop-new-card__form form-new" id="formNewCard" action="#">
                     <div class="form-new__block">
-                      <label for="formTitle" class="subttl">Название задачи</label>
-                      <input class="form-new__input" type="text" name="name" id="formTitle" placeholder="Введите название задачи..." autofocus>
+                      <label htmlFor="formTitle" class="subttl">Название задачи</label>
+                      <input
+                        className="form-new__input"
+                        type="text"
+                        name="name"
+                        id="formTitle"
+                        placeholder="Введите название задачи..."
+                        autoFocus=""
+                      />
                     </div>
                     <div class="form-new__block">
-                      <label for="textArea" class="subttl">Описание задачи</label>
-                      <textarea class="form-new__area" name="text" id="textArea" placeholder="Введите описание задачи..."></textarea>
+                      <label htmlFor="textArea" class="subttl">Описание задачи</label>
+                      <textarea
+                        className="form-new__area"
+                        name="text"
+                        id="textArea"
+                        placeholder="Введите описание задачи..."
+                        defaultValue={""}
+                      />
                     </div>
                   </form>
                   <div class="pop-new-card__calendar calendar">
@@ -51,12 +68,22 @@ function App() {
                         <div class="calendar__month">Сентябрь 2023</div>
                         <div class="nav__actions">
                           <div class="nav__action" data-action="prev">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={6}
+                              height={11}
+                              viewBox="0 0 6 11"
+                            >
                               <path d="M5.72945 1.95273C6.09018 1.62041 6.09018 1.0833 5.72945 0.750969C5.36622 0.416344 4.7754 0.416344 4.41218 0.750969L0.528487 4.32883C-0.176162 4.97799 -0.176162 6.02201 0.528487 6.67117L4.41217 10.249C4.7754 10.5837 5.36622 10.5837 5.72945 10.249C6.09018 9.9167 6.09018 9.37959 5.72945 9.04727L1.87897 5.5L5.72945 1.95273Z" />
                             </svg>
                           </div>
                           <div class="nav__action" data-action="next">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={6}
+                              height={11}
+                              viewBox="0 0 6 11"
+                            >
                               <path d="M0.27055 9.04727C-0.0901833 9.37959 -0.0901832 9.9167 0.27055 10.249C0.633779 10.5837 1.2246 10.5837 1.58783 10.249L5.47151 6.67117C6.17616 6.02201 6.17616 4.97799 5.47151 4.32883L1.58782 0.75097C1.2246 0.416344 0.633778 0.416344 0.270549 0.75097C-0.0901831 1.0833 -0.090184 1.62041 0.270549 1.95273L4.12103 5.5L0.27055 9.04727Z" />
                             </svg>
                           </div>
@@ -111,10 +138,10 @@ function App() {
                         </div>
                       </div>
 
-                      <input type="hidden" id="datepick_value" value="08.09.2023">
-                        <div class="calendar__period">
-                          <p class="calendar__p date-end">Выберите срок исполнения <span class="date-control"></span>.</p>
-                        </div>
+                      <input type="hidden" id="datepick_value" defaultValue="08.09.2023" />
+                      <div class="calendar__period">
+                        <p class="calendar__p date-end">Выберите срок исполнения <span class="date-control"></span>.</p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -132,7 +159,7 @@ function App() {
                     </div>
                   </div>
                 </div>
-                <button class="form-new__create _hover01" id="btnCreate">Создать задачу</button>
+                <button className="form-new__create _hover01" id="btnCreate">Создать задачу</button>
               </div>
             </div>
           </div>
@@ -171,7 +198,7 @@ function App() {
                 <div class="pop-browse__wrap">
                   <form class="pop-browse__form form-browse" id="formBrowseCard" action="#">
                     <div class="form-browse__block">
-                      <label for="textArea01" class="subttl">Описание задачи</label>
+                      <label htmlFor="textArea01" class="subttl">Описание задачи</label>
                       <textarea class="form-browse__area" name="text" id="textArea01" readonly placeholder="Введите описание задачи..."></textarea>
                     </div>
                   </form>
@@ -182,12 +209,28 @@ function App() {
                         <div class="calendar__month">Сентябрь 2023</div>
                         <div class="nav__actions">
                           <div class="nav__action" data-action="prev">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
-                              <path d="M5.72945 1.95273C6.09018 1.62041 6.09018 1.0833 5.72945 0.750969C5.36622 0.416344 4.7754 0.416344 4.41218 0.750969L0.528487 4.32883C-0.176162 4.97799 -0.176162 6.02201 0.528487 6.67117L4.41217 10.249C4.7754 10.5837 5.36622 10.5837 5.72945 10.249C6.09018 9.9167 6.09018 9.37959 5.72945 9.04727L1.87897 5.5L5.72945 1.95273Z" />
-                            </svg>
+                            <>
+                              <button className="form-new__create _hover01" id="btnCreate">
+                                Создать задачу
+                              </button>
+                              a
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={6}
+                                height={11}
+                                viewBox="0 0 6 11"
+                              >
+                                <path d="M5.72945 1.95273C6.09018 1.62041 6.09018 1.0833 5.72945 0.750969C5.36622 0.416344 4.7754 0.416344 4.41218 0.750969L0.528487 4.32883C-0.176162 4.97799 -0.176162 6.02201 0.528487 6.67117L4.41217 10.249C4.7754 10.5837 5.36622 10.5837 5.72945 10.249C6.09018 9.9167 6.09018 9.37959 5.72945 9.04727L1.87897 5.5L5.72945 1.95273Z" />
+                              </svg>
+                            </>
                           </div>
                           <div class="nav__action" data-action="next">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={6}
+                              height={11}
+                              viewBox="0 0 6 11"
+                            >
                               <path d="M0.27055 9.04727C-0.0901833 9.37959 -0.0901832 9.9167 0.27055 10.249C0.633779 10.5837 1.2246 10.5837 1.58783 10.249L5.47151 6.67117C6.17616 6.02201 6.17616 4.97799 5.47151 4.32883L1.58782 0.75097C1.2246 0.416344 0.633778 0.416344 0.270549 0.75097C-0.0901831 1.0833 -0.090184 1.62041 0.270549 1.95273L4.12103 5.5L0.27055 9.04727Z" />
                             </svg>
                           </div>
@@ -242,10 +285,10 @@ function App() {
                         </div>
                       </div>
 
-                      <input type="hidden" id="datepick_value" value="08.09.2023">
-                        <div class="calendar__period">
-                          <p class="calendar__p date-end">Срок исполнения: <span class="date-control">09.09.23</span></p>
-                        </div>
+                      <input type="hidden" id="datepick_value" value="08.09.2023" />
+                      <div class="calendar__period">
+                        <p class="calendar__p date-end">Срок исполнения: <span class="date-control">09.09.23</span></p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -282,21 +325,25 @@ function App() {
           <div class="container">
             <div class="header__block">
               <div class="header__logo _show _light">
-                <a href="" target="_self"><img src="images/logo.png" alt="logo"></a>
+                <a href="" target="_self">
+                  <img src="images/logo.png" alt="logo" />
+                </a>
               </div>
               <div class="header__logo _dark">
-                <a href="" target="_self"><img src="images/logo_dark.png" alt="logo"></a>
+                <a href="" target="_self">
+                  <img src="images/logo_dark.png" alt="logo" />
+                </a>
               </div>
               <nav class="header__nav">
                 <button class="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
                 <a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
                 <div class="header__pop-user-set pop-user-set" id="user-set-target">
-                  <!-- <a href="">x</a> -->
+                  {/*<!-- <a href="">x</a> -->*/}
                   <p class="pop-user-set__name">Ivan Ivanov</p>
                   <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
                   <div class="pop-user-set__theme">
                     <p>Темная тема</p>
-                    <input type="checkbox" class="checkbox" name="checkbox">
+                    <input type="checkbox" class="checkbox" name="checkbox" />
                   </div>
                   <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
                 </div>
