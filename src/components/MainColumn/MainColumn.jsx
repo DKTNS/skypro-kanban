320a78defa80b165cffa2d9597caa7b23aa0cd89
DKTNS <zigindmitry@gmail.com>
2024-02-20 13:13:36 +0300

    переделки

---
+++ src/components/MainColumn/MainColumn.jsx
@@ -0,0 +1,18 @@
+import Card from './components/Card/Card.jsx'
+
+export function MainColumn ({ title }) {
+  return (
+    <div class="main__column column">
+      <div class="column__title">
+        <p>{title}</p>
+      </div>
+      <div class="cards">
+        <Card StatusTask={"Web Design"} NameTask={"Задача №1"} />
+        <Card StatusTask={"Web Design"} NameTask={"Задача №2"} />
+        <Card StatusTask={"Research"} NameTask={"Задача №3"} />
+        <Card StatusTask={"Copywriting"} NameTask={"Задача №4"} />
+        <Card StatusTask={"Web Design"} NameTask={"Задача №5"} />
+      </div>
+    </div>
+  )
+}
