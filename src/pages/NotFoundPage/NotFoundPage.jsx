148b825c3e2b0ae84c5602ec89daf488002314cb
DKTNS <zigindmitry@gmail.com>
2024-04-17 08:45:31 +0300

    Исправлено Sign. Осталась ошибка notFounPage

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import * as NF from "./NotFoundPage.styled";
+import "./style.css";
+
+<NF.BodyNfp>
+  <NF.ErrorContainer>
+    <NF.H1Font> 404 </NF.H1Font>
+    <NF.p>Oops! The page you're looking for is not here.</NF.p>
+    <a href="#">Go Back to Home</a>
+  </NF.ErrorContainer>
+</NF.BodyNfp>;
